3ad0b49248e4ec6680f591e5d2dd8f9c1783611c
David Mittelman <dbmittelman@gmail.com>
2020-06-04 21:12:06 -0700

    Return the operating system name

--- src/system.cpp
+++ src/system.cpp
@@ -1,12 +1,15 @@
+#include "system.h"
+
 #include <unistd.h>
+
 #include <cstddef>
 #include <set>
 #include <string>
 #include <vector>
 
+#include "linux_parser.h"
 #include "process.h"
 #include "processor.h"
-#include "system.h"
 
 using std::set;
 using std::size_t;
@@ -25,8 +28,8 @@ std::string System::Kernel() { return string(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
-std::string System::OperatingSystem() { return string(); }
+// DONE: Return the operating system name
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
